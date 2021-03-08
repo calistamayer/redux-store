57bd55c5f6bc0468660c8fbe7d7c36660fbe40fb
Calista Mayer <calista.a.mayer@gmail.com>
2021-03-07 19:21:10 -0800

    update index.js to work with redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('app')
 );
 
 // If you want your app to work offline and load faster, you can change
